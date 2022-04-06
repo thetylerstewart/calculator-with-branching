9537e4d078c509dbfcb921232d984e3cf4778d65
Tyler Stewart <mr.tstew@gmail.com>
2022-04-06 08:23:06 -0700

    add branching to calculator app

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+//business logic
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+
+
+
+//user interface
+$(document).ready(function () {
+  $("form#calculator").submit(function () {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
